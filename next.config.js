87abf2381eaa9cc0db835678e12e9000d11517cb
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2022-09-06 12:16:53 -0500

    Home page only avaialable

--- next.config.js
+++ next.config.js
@@ -1,7 +1,33 @@
 /** @type {import('next').NextConfig} */
+
+/*module.exports = {
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config
+  }
+  
+}*/
+
+
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  // swcMinify: true,
+  swcMinify: false, // Required to fix: https://nextjs.org/docs/messages/failed-loading-swc
+  async rewrites() {
+    return [
+      {
+        source: "/home",
+        destination: "/",
+      },
+      // { source: '/categories', destination: '/c },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
