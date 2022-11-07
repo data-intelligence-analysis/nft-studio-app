db15f64f9ad59ed96ebb0780e01d6d36f39dc5e3
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2022-11-07 14:41:13 -0600

    deployment fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/home.module.css'
 import FrontPage from '../components/FrontPage'
 import NavBar from '../components/NavBar'
