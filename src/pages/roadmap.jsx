f6d07e7f3ee64f344414720ebd7bed31b23859c3
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2022-11-14 15:01:59 -0600

    setup responsive layout for roadmap pics and items

--- src/pages/roadmap.jsx
+++ src/pages/roadmap.jsx
@@ -12,8 +12,8 @@ export default function Gaming() {
       </Head>
       <NavBar bgFormat={"bg-[#343333]"} />
       <div className="bg-[var(--tw-main-bg-color)]">
-        <div className="px-4 py-10 sm:py-10 grid lg:py-12 sm:px-6 sm:grid sm:grid-cols-8 md:grid-cols-10 lg:grid-cols-12 min-h-screen xl:max-w-screen-2xl sm:gap-2 lg:gap-4">
-          <div className="h-full py-6 sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-span-10">
+        <div className="px-4 py-10 sm:py-10 grid lg:py-12 sm:px-6 sm:grid sm:grid-cols-8 md:grid-cols-10 lg:grid-cols-12 min-h-full max-w-full xl:max-w-screen-2xl sm:gap-2 lg:gap-4 mx-auto">
+          <div className="h-full py-6 sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-span-12">
             <div className="h-full pt-4 grid sm:grid-cols-2 place-items-center">
               <div className= "grid place-items-center object-contain object-center py-4 px-2 bg-transparent">
                 <Image 
