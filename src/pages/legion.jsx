e37521492a0be8de3af26de46cd3c9b4c9555de3
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2022-11-17 17:28:25 -0800

    ted mission and utility done

--- src/pages/legion.jsx
+++ src/pages/legion.jsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
+import Link from 'next/link'
 
 export default function Legion() {
   return (
@@ -27,14 +28,21 @@ export default function Legion() {
                 <div className="mt-10 overflow-auto">
                   <h1 className="text-xl sm:text-3xl tracking-wider font-bold">Utility</h1>
                   <div className="font-sans">
-                    <p className="pt-5">If you are new to Solana space, please head over to this <a href ="https://youtu.be/mJiXmsiLS3w" target="_blank" rel="noreferrer" className="font-color focus pad-top font-weight">link</a> to learn how to 
+                    <p className="pt-3">If you are new to Solana space, please head over to this <a href ="https://youtu.be/mJiXmsiLS3w" target="_blank" rel="noreferrer" className="font-['Ranchers'] font-bold tracking-wide">link</a> to learn how to 
                     create and connect your Solana wallet, 
-                    or head on over to our <a href ="http://discord.gg/N5wB8JTBBS" target="_blank" rel="noreferrer" className="font-color focus pad-top font-weight">Discord</a> to request for assistance, 
-                    address any concerns, meet members of the community, 
-                    or learn more about this new trailblazing ecosystem. </p>
-                    <p className="pt-5">Our utility is centered around our NFTs and tokens that offer holder (membership) benefits. 
-                    Currently, those benefits would be accessible through our <a href ="https://metateds.com" noreferrer className="font-color focus pad-top font-weight" target="_blank" rel="noreferrer">blog platform</a>, which include but not limited to the ability for each holder to build a
-                    personalized page to feature their blogs, analyzed current engagement metrics of their blogs, view other blogs, and follow and interact with other members.</p>
+                    or head on over to our <a href ="http://discord.gg/N5wB8JTBBS" target="_blank" rel="noreferrer" className="font-['Ranchers'] font-bold tracking-wide">Discord</a> to request for assistance, 
+                    meet members of the community, 
+                    and learn more about our platform. </p>
+                    <p className="pt-3">Our utility is centered around our NFTs and tokens that offer holder (membership) benefits. 
+                    Currently, those benefits would be accessible through our <a href ="https://metateds.com" noreferrer className="font-['Ranchers'] font-bold tracking-wide" target="_blank" rel="noreferrer">blog platform</a>, which include but not limited to the ability for each holder to build a
+                    personalized page to feature their blogs, publish content and make posts, view other blogs, and follow and interact with other members.</p>
+                    <p className="pt-3">
+                      You can access our blog through our ecosystem as well by connecting to our <Link href="/utility" noreferrer className="font-['Ranchers'] font-bold tracking-wide">Utility</Link> page. with your Solana wallet.
+                    </p>
+                    <p className="pt-3">
+                      Eventually, we would mutate those NFTS into gamefied utility tokens  
+                      utilized in our <a href="https://metateds.com/gaming" className="font-['Ranchers'] font-bold tracking-wide">Gaming</a>
+                    </p>
                   </div>
                 </div>
               </div>
