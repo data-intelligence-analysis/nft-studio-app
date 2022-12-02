fd8c94be20d622dd8ae34fa7e3427df6cd94d942
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2022-12-02 00:30:40 -0600

    resolve from later

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
+    darkMode: "class",
     content: [
       "./src/**/*.{js,jsx,ts,tsx}",
     //'./pages/**/*.{js,ts,jsx,tsx}',
@@ -10,39 +11,38 @@ module.exports = {
 
     ],
     theme: {
-      /*screens: {
-        'sm': {'min': '640px', 'max': '767px'},
-        // => @media (min-width: 640px and max-width: 767px) { ... }
-
-        'md': {'min': '768px', 'max': '1023px'},
-        // => @media (min-width: 768px and max-width: 1023px) { ... }
-
-        'lg': {'min': '1024px', 'max': '1279px'},
-        // => @media (min-width: 1024px and max-width: 1279px) { ... }
-
-        'xl': {'min': '1280px', 'max': '1535px'},
-        // => @media (min-width: 1280px and max-width: 1535px) { ... }
-
-        '2xl': {'min': '1536px'},
-        // => @media (min-width: 1536px) { ... }
-      },*/
-      /*colors: {
-        'blue': '#1fb6ff',
-        'pink': '#ff49db',
-        'orange': '#ff7849',
-        'green': '#13ce66',
-        'gray-dark': '#273444',
-        'gray': '#8492a6',
-        'gray-light': '#d3dce6',
-        'metateds-purple': '#4e44ce',
-        'metateds-orange': '#EAA640'
-      },*/
-      
       extend: {
+          /*screens: {
+          'sm': {'min': '640px', 'max': '767px'},
+          // => @media (min-width: 640px and max-width: 767px) { ... }
+
+          'md': {'min': '768px', 'max': '1023px'},
+          // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+          'lg': {'min': '1024px', 'max': '1279px'},
+          // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+          'xl': {'min': '1280px', 'max': '1535px'},
+          // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+          '2xl': {'min': '1536px'},
+          // => @media (min-width: 1536px) { ... }
+        },*/
+        /*colors: {
+          'blue': '#1fb6ff',
+          'pink': '#ff49db',
+          'orange': '#ff7849',
+          'green': '#13ce66',
+          'gray-dark': '#273444',
+          'gray': '#8492a6',
+          'gray-light': '#d3dce6',
+          'metateds-purple': '#4e44ce',
+          'metateds-orange': '#EAA640'
+        },*/
         gridTemplateColumns: {
+          2: "repeat(2, minmax(0, 1fr))",
           13: "repeat(13, minmax(0, 1fr))",
           16: "repeat(16, minmax(0, 1fr))",
-          2: "repeat(2, minmax(0, 1fr))"
         },
       },
     },
