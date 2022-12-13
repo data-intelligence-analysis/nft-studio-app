cca05a1f7ab0670bd179935d1e362f6a40cf4f8f
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2022-12-12 19:47:34 -0600

    remove dependencies "

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,8 +29,8 @@ module.exports = {
       },
     },
     plugins: [
-      require("@tailwindcss/forms"),
-      require("@tailwindcss/line-clamp"),
-      require("@tailwindcss/typography"),
+      //require("@tailwindcss/forms"),
+      //require("@tailwindcss/line-clamp"),
+      //require("@tailwindcss/typography"),
     ],
 }
