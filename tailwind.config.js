7c85fde7b121eeacf71a032ec75f2752543dd7b4
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2022-12-13 22:53:36 -0600

    added some fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,18 +9,23 @@ module.exports = {
 
     ],
     theme: {
+    /*colors: {
+        'blue': '#1fb6ff',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+        'metateds-purple': '#4e44ce',
+        'metateds-orange': '#EAA640'
+      },*/
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+        mono: ['SFMono-Regular', 'monospace']
+      },
       extend: {
-        /*colors: {
-          'blue': '#1fb6ff',
-          'pink': '#ff49db',
-          'orange': '#ff7849',
-          'green': '#13ce66',
-          'gray-dark': '#273444',
-          'gray': '#8492a6',
-          'gray-light': '#d3dce6',
-          'metateds-purple': '#4e44ce',
-          'metateds-orange': '#EAA640'
-        },*/
         gridTemplateColumns: {
           2: "repeat(2, minmax(0, 1fr))",
           13: "repeat(13, minmax(0, 1fr))",
