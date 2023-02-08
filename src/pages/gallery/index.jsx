16b8741e5d12f621842279a1cb5ee569c1d189a6
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2023-02-08 10:58:49 -0600

    optimizing stage 1

---
+++ src/pages/gallery/index.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Link from 'next/link';
+import Image from 'next/image';
+import {buildUrl} from 'cloudinary-build-url';
+
+export default function Home() {
+  return (
+    <>
+    </>
+  )
+}
