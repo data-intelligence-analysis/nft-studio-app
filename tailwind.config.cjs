9180d08fa615ea3da4ebc241409ef5bc11066f33
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2023-02-11 17:12:57 -0600

    reinstalled tailwind and autoprefixer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,41 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-    darkMode: "class",
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    //'./pages/**/*.{js,ts,jsx,tsx}',
-    //'./components/**/*.{js,ts,jsx,tsx}',
-
-    ],
-    theme: {
-    /*colors: {
-        'blue': '#1fb6ff',
-        'pink': '#ff49db',
-        'orange': '#ff7849',
-        'green': '#13ce66',
-        'gray-dark': '#273444',
-        'gray': '#8492a6',
-        'gray-light': '#d3dce6',
-        'metateds-purple': '#4e44ce',
-        'metateds-orange': '#EAA640'
-      },*/
-      fontFamily: {
-        sans: ['Graphik', 'sans-serif'],
-        serif: ['Merriweather', 'serif'],
-        mono: ['SFMono-Regular', 'monospace']
-      },
-      extend: {
-        gridTemplateColumns: {
-          2: "repeat(2, minmax(0, 1fr))",
-          13: "repeat(13, minmax(0, 1fr))",
-          16: "repeat(16, minmax(0, 1fr))",
-        },
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+      mono: ['SFMono-Regular', 'monospace']
+    },
+    extend: {
+      gridTemplateColumns: {
+        2: "repeat(2, minmax(0, 1fr))",
+        13: "repeat(13, minmax(0, 1fr))",
+        16: "repeat(16, minmax(0, 1fr))",
       },
     },
-    plugins: [
-      //require("@tailwindcss/forms"),
-      //require("@tailwindcss/line-clamp"),
-      //require("@tailwindcss/typography"),
-    ],
+  },
 }
