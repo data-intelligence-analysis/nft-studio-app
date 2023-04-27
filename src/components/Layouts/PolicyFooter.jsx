32e48b07ba2d6a31cb5095aac2854c57cf2daae9
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2023-04-27 14:55:31 -0500

    policy templates

---
+++ src/components/Layouts/PolicyFooter.jsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react'
+import SubscriberForm from '../Mailing/SubscriberForm'
+export const PolicyFooter = () => {
+  return (
+    <div>
+      <footer>
+        <SubscriberForm />
+      </footer>
+    </div>
+  )
+}
